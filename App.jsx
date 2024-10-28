917c19e53570a13f4feb9f222b8939cfa8e72458
Oussamabassor <adambassor0@gmail.com>
2024-10-28 16:59:20 +0100

    first commit

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ScoreBoard from './scoreboard';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <ScoreBoard />
+        </div>
+    );
+}
+
+export default App;
